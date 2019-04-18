98f06fb98fad4e526de9b739c8db1f66793c36ab
Ka Hou Lou <kahoulou318@gmail.com>
2019-04-17 20:22:44 -0400

    Fixed Path

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     # view.menu_sections deals with all operations relating to menu sections as a whole.
     # This includes getting all menu sections and adding a new section to the menu.
     path('menusection/', views.menu_sections, name='Menu Sections'),
-    path('menusection', RedirectView.as_view(url='menusection/')),
+    path('menusection', views.menu_sections, name='Menu Sections'),
 
     # view.specific_menu_section deals with all operations relating to a specific menu section.
     # This includes retrieving the specific menu section, editing, and deleting it.
